9675b257954da14706c86634f5a68fcfa3cb369f
Reto Spoerri <spoerri@ludic.ch>
2015-01-24 17:31:00 +0100

    fixed mecanim animation

--- ReverSciFi/Assets/Scripts/PlayerControl.cs
+++ ReverSciFi/Assets/Scripts/PlayerControl.cs
@@ -75,7 +75,7 @@ public class PlayerControl : MonoBehaviour
 		}
 
 		// The Speed animator parameter is set to the absolute value of the horizontal input.
-		anim.SetFloat("Speed", Mathf.Abs(h));
+//		anim.SetFloat("Speed", Mathf.Abs(h));
 
 		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
 		if(h * rigidbody2D.velocity.x < maxSpeed)

17f7edf0f88c62872bc930891d8aa141e276d850
David Stark <david.stark@zarkonnen.com>
2015-01-24 23:11:20 +0100

    Switches

---
+++ ReverSciFi/Assets/LeverSwitch.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeverSwitch : MonoBehaviour {
+	public bool on = false;
+
+	private Animator anim;
+	private float timeSinceSwitch;
+
+	void Update() {
+		timeSinceSwitch += Time.deltaTime;
+	}
+
+	void Start() {
+		anim = transform.GetComponentInChildren<Animator>();
+		anim.SetBool ("On", on);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player" && timeSinceSwitch > 1) {
+			on = !on;
+			anim.SetBool ("On", on);
+			timeSinceSwitch = 0;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+
+	}
+}

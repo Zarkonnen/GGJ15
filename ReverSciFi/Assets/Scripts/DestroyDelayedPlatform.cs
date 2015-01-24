d93da34c55d255e877e40884be4d8c299a6b14dd
David Stark <david.stark@zarkonnen.com>
2015-01-24 17:22:25 +0100

    Crack anim

--- ReverSciFi/Assets/Scripts/DestroyDelayedPlatform.cs
+++ ReverSciFi/Assets/Scripts/DestroyDelayedPlatform.cs
@@ -2,16 +2,18 @@
 using System.Collections;
 
 public class DestroyDelayedPlatform : MonoBehaviour {
-
-	public float destroyTimer = 3.0f;
-
+	public float destroyTimer = 0.6f;
+	private bool destroying;
 
 	void OnCollisionEnter2D (Collision2D collision) {
 //		Debug.Log ("DestroyDelayedPlatform.OnCollisionEnter: "+gameObject.name+" collision.collider.tag "+collision.collider.tag);
 
-		if (collision.collider.tag == "Player") {
+		if (!destroying && collision.collider.tag == "Player") {
 			Invoke("Destroy", destroyTimer);
+			transform.FindChild("Crack").GetComponentInChildren<Animator>().SetTrigger("Start");
+			destroying = true;
 		}
+
 		/*foreach (ContactPoint contact in collision.contacts) {
 			Debug.DrawRay(contact.point, contact.normal, Color.white);
 		}*/

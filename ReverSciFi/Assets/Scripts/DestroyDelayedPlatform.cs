b81b356acbf348b7c672e0e80b46f5381c1c0837
Reto Spoerri <spoerri@ludic.ch>
2015-01-24 12:59:47 +0100

    added uniq-id-generator for objects

--- ReverSciFi/Assets/Scripts/DestroyDelayedPlatform.cs
+++ ReverSciFi/Assets/Scripts/DestroyDelayedPlatform.cs
@@ -5,12 +5,17 @@ public class DestroyDelayedPlatform : MonoBehaviour {
 
 	public float destroyTimer = 3.0f;
 
-	void OnCollisionEnter2D () { //Collision2D collision) {
-		Debug.Log ("DestroyDelayedPlatform.OnCollisionEnter: "+gameObject.name);
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		Debug.Log ("DestroyDelayedPlatform.OnCollisionEnter: "+gameObject.name+" collision.collider.tag "+collision.collider.tag);
+
+		if (collision.collider.tag == "Player") {
+			Invoke("Destroy", destroyTimer);
+		}
 		/*foreach (ContactPoint contact in collision.contacts) {
 			Debug.DrawRay(contact.point, contact.normal, Color.white);
 		}*/
-		Invoke("Destroy", destroyTimer);
+
 	}
 	
 

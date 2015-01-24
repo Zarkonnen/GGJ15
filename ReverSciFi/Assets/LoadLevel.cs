02736c9a7c044191517302af34b26d40a84bb727
Reto Spoerri <spoerri@ludic.ch>
2015-01-24 22:24:52 +0100

    intro

---
+++ ReverSciFi/Assets/LoadLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	public int level;
+
+	public void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Load ();
+		}
+	}
+
+	public void Load () {
+		Application.LoadLevel(level);
+	}
+}

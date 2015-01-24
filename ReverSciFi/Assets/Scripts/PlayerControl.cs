110525377f7bdb3f7c4190e6cc645ffeabe3d322
Kaspar Manz <kaspar@xeophin.net>
2015-01-24 23:04:30 +0100

    First timid steps in creating a level.

--- ReverSciFi/Assets/Scripts/PlayerControl.cs
+++ ReverSciFi/Assets/Scripts/PlayerControl.cs
@@ -4,185 +4,195 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour
 {
 
-	public KeyCode leftKey = KeyCode.A;
-	public KeyCode rightKey = KeyCode.D;
-	public KeyCode jumpKey = KeyCode.Space;
-	public KeyCode sneakKey = KeyCode.LeftShift;
+  public KeyCode leftKey = KeyCode.A;
+  public KeyCode rightKey = KeyCode.D;
+  public KeyCode jumpKey = KeyCode.Space;
+  public KeyCode sneakKey = KeyCode.LeftShift;
 
-	public float sneakVelocity = 0.5f;
-	public float walkVelocity = 1.5f;
+  public float sneakVelocity = 0.5f;
+  public float walkVelocity = 1.5f;
 
-	public float animatedVelocity = 0.0f;
+  public float animatedVelocity = 0.0f;
 
-	[HideInInspector]
-	public bool facingRight = false;			// For determining which way the player is currently facing.
-	[HideInInspector]
-	public bool jump = false;				// Condition for whether the player should jump.
-
-
-	public float moveForce = 365f;			// Amount of force added to move the player left and right.
-	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
-	public AudioClip[] jumpClips;			// Array of clips for when the player jumps.
-	public float jumpForce = 1000f;			// Amount of force added when the player jumps.
-	public AudioClip[] taunts;				// Array of clips for when the player taunts.
-	public float tauntProbability = 50f;	// Chance of a taunt happening.
-	public float tauntDelay = 1f;			// Delay for when the taunt should happen.
+  [HideInInspector]
+  public bool facingRight = false;
+  // For determining which way the player is currently facing.
+  [HideInInspector]
+  public bool jump = false;
+  // Condition for whether the player should jump.
 
 
-	private int tauntIndex;					// The index of the taunts array indicating the most recent taunt.
-	private Transform groundCheck;			// A position marking where to check if the player is grounded.
-	private bool grounded = false;			// Whether or not the player is grounded.
-	private Animator anim;					// Reference to the player's animator component.
+  public float moveForce = 365f;
+  // Amount of force added to move the player left and right.
+  public float maxSpeed = 5f;
+  // The fastest the player can travel in the x axis.
+  public AudioClip[] jumpClips;
+  // Array of clips for when the player jumps.
+  public float jumpForce = 1000f;
+  // Amount of force added when the player jumps.
+  public AudioClip[] taunts;
+  // Array of clips for when the player taunts.
+  public float tauntProbability = 50f;
+  // Chance of a taunt happening.
+  public float tauntDelay = 1f;
+  // Delay for when the taunt should happen.
 
 
-	void Awake()
-	{
-		// Setting up references.
-		groundCheck = transform.Find("groundCheck");
-		anim = GetComponent<Animator>();
-
-		rigidbody2D.GetComponent<CircleCollider2D>().sharedMaterial.friction = 1.0f;
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
+  private int tauntIndex;
+  // The index of the taunts array indicating the most recent taunt.
+  private Transform groundCheck;
+  // A position marking where to check if the player is grounded.
+  private bool grounded = false;
+  // Whether or not the player is grounded.
+  private Animator anim;
+  // Reference to the player's animator component.
 
 
-	void Update()
-	{
-		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
+  void Awake ()
+  {
+    // Setting up references.
+    groundCheck = transform.Find ("groundCheck");
+    anim = GetComponent<Animator> ();
 
-		// If the jump button is pressed and the player is grounded then the player should jump.
-		if(Input.GetKeyDown(jumpKey) && grounded)
-			jump = true;
-	}
+    rigidbody2D.GetComponent<CircleCollider2D> ().sharedMaterial.friction = 1.0f;
+    Vector3 theScale = transform.localScale;
+    theScale.x *= -1;
+    transform.localScale = theScale;
+  }
 
 
-	void FixedUpdate ()
-	{
-		// Cache the horizontal input.
-		//float h = Input.GetAxis("Horizontal");
-		float h = 0;
-		if (Input.GetKey(leftKey)) {
-			if (Input.GetKey(sneakKey)) {
-				h = -walkVelocity;
-			} else {
-				h = -sneakVelocity;
-			}
-		}
-		if (Input.GetKey(rightKey)) {
-			if (Input.GetKey(sneakKey)) {
-				h = walkVelocity;
-			} else {
-				h = sneakVelocity;
-			}
-		}
+  void Update ()
+  {
+    // The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+    grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));  
 
-		if (animatedVelocity != 0) {
-			h = animatedVelocity;
-		}
+    // If the jump button is pressed and the player is grounded then the player should jump.
+    if (Input.GetKeyDown (jumpKey) && grounded)
+      jump = true;
+  }
 
-		if (Mathf.Abs(h) > 0.1f) {
-			rigidbody2D.GetComponent<CircleCollider2D>().sharedMaterial.friction = 1.0f;
-			//h = h / h * Mathf.Sign(h);
-		} else {
-			rigidbody2D.GetComponent<CircleCollider2D>().sharedMaterial.friction = 10.0f;
-		}
 
-		// The Speed animator parameter is set to the absolute value of the horizontal input.
-		anim.SetFloat("Speed", Mathf.Abs(h));
+  void FixedUpdate ()
+  {
+    // Cache the horizontal input.
+    //float h = Input.GetAxis("Horizontal");
+    float h = 0;
+    if (Input.GetKey (leftKey)) {
+      if (Input.GetKey (sneakKey)) {
+        h = -walkVelocity;
+      } else {
+        h = -sneakVelocity;
+      }
+    }
+    if (Input.GetKey (rightKey)) {
+      if (Input.GetKey (sneakKey)) {
+        h = walkVelocity;
+      } else {
+        h = sneakVelocity;
+      }
+    }
 
-		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-		if(h * rigidbody2D.velocity.x < maxSpeed)
+    if (animatedVelocity != 0) {
+      h = animatedVelocity;
+    }
+
+    if (Mathf.Abs (h) > 0.1f) {
+      rigidbody2D.GetComponent<CircleCollider2D> ().sharedMaterial.friction = 1.0f;
+      //h = h / h * Mathf.Sign(h);
+    } else {
+      rigidbody2D.GetComponent<CircleCollider2D> ().sharedMaterial.friction = 10.0f;
+    }
+
+    // The Speed animator parameter is set to the absolute value of the horizontal input.
+    anim.SetFloat ("Speed", Mathf.Abs (h));
+
+    // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+    if (h * rigidbody2D.velocity.x < maxSpeed)
 			// ... add a force to the player.
-			rigidbody2D.AddForce(Vector2.right * h * moveForce);
+			rigidbody2D.AddForce (Vector2.right * h * moveForce);
 
-		// If the player's horizontal velocity is greater than the maxSpeed...
-		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+    // If the player's horizontal velocity is greater than the maxSpeed...
+    if (Mathf.Abs (rigidbody2D.velocity.x) > maxSpeed)
 			// ... set the player's velocity to the maxSpeed in the x axis.
-			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+			rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
 
 
 
-		// If the input is moving the player right and the player is facing left...
-		if(h > 0 && !facingRight)
+    // If the input is moving the player right and the player is facing left...
+    if (h > 0 && !facingRight)
 			// ... flip the player.
-			Flip();
+			Flip ();
 		// Otherwise if the input is moving the player left and the player is facing right...
-		else if(h < 0 && facingRight)
+		else if (h < 0 && facingRight)
 			// ... flip the player.
-			Flip();
+			Flip ();
 
-		// If the player should jump...
-		if(jump)
-		{
-			// Set the Jump animator trigger parameter.
-			anim.SetTrigger("Jump");
+    // If the player should jump...
+    if (jump) {
+      // Set the Jump animator trigger parameter.
+      //anim.SetTrigger("Jump");
 
-			// Play a random jump audio clip.
-			int i = Random.Range(0, jumpClips.Length);
-			//AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
+      // Play a random jump audio clip.
+      int i = Random.Range (0, jumpClips.Length);
+      //AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
 
-			// Add a vertical force to the player.
-			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+      // Add a vertical force to the player.
+      rigidbody2D.AddForce (new Vector2 (0f, jumpForce));
 
-			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
-			jump = false;
-		}
-	}
+      // Make sure the player can't jump again until the jump conditions from Update are satisfied.
+      jump = false;
+    }
+  }
 	
 	
-	/*void OnCollisionEnter2D (Collision2D collision) {
+  /*void OnCollisionEnter2D (Collision2D collision) {
 		Debug.Log ("PlayerControler.OnCollisionEnter: "+gameObject.name);
 	}*/
 
-	void Flip ()
-	{
-		// Switch the way the player is labelled as facing.
-		facingRight = !facingRight;
+  void Flip ()
+  {
+    // Switch the way the player is labelled as facing.
+    facingRight = !facingRight;
 
-		// Multiply the player's x local scale by -1.
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
-
-
-	public IEnumerator Taunt()
-	{
-		// Check the random chance of taunting.
-		float tauntChance = Random.Range(0f, 100f);
-		if(tauntChance > tauntProbability)
-		{
-			// Wait for tauntDelay number of seconds.
-			yield return new WaitForSeconds(tauntDelay);
-
-			// If there is no clip currently playing.
-			if(!audio.isPlaying)
-			{
-				// Choose a random, but different taunt.
-				tauntIndex = TauntRandom();
-
-				// Play the new taunt.
-				audio.clip = taunts[tauntIndex];
-				audio.Play();
-			}
-		}
-	}
+    // Multiply the player's x local scale by -1.
+    Vector3 theScale = transform.localScale;
+    theScale.x *= -1;
+    transform.localScale = theScale;
+  }
 
 
-	int TauntRandom()
-	{
-		// Choose a random index of the taunts array.
-		int i = Random.Range(0, taunts.Length);
+  public IEnumerator Taunt ()
+  {
+    // Check the random chance of taunting.
+    float tauntChance = Random.Range (0f, 100f);
+    if (tauntChance > tauntProbability) {
+      // Wait for tauntDelay number of seconds.
+      yield return new WaitForSeconds (tauntDelay);
 
-		// If it's the same as the previous taunt...
-		if(i == tauntIndex)
+      // If there is no clip currently playing.
+      if (!audio.isPlaying) {
+        // Choose a random, but different taunt.
+        tauntIndex = TauntRandom ();
+
+        // Play the new taunt.
+        audio.clip = taunts [tauntIndex];
+        audio.Play ();
+      }
+    }
+  }
+
+
+  int TauntRandom ()
+  {
+    // Choose a random index of the taunts array.
+    int i = Random.Range (0, taunts.Length);
+
+    // If it's the same as the previous taunt...
+    if (i == tauntIndex)
 			// ... try another random taunt.
-			return TauntRandom();
-		else
+			return TauntRandom ();
+    else
 			// Otherwise return this index.
 			return i;
-	}
+  }
 }

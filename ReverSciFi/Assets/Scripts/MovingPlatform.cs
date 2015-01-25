669c7f3baa38285ab3f1c86e5152fdf96df2f3e0
Reto Spoerri <spoerri@ludic.ch>
2015-01-25 12:10:36 +0100

    moving platform speed added

--- ReverSciFi/Assets/Scripts/MovingPlatform.cs
+++ ReverSciFi/Assets/Scripts/MovingPlatform.cs
@@ -6,14 +6,15 @@ public class MovingPlatform : MonoBehaviour {
 	//public Vector3 startPos;
 	//public Vector3 endPos;
 	private Vector3 sourcePos;
-	public Vector3 offsetPos;
+	public Vector3 offsetPos = Vector3.up;
+	public float movementTime = 2.0f;
 
 	void Start () {
 		sourcePos = transform.position;
 	}
 
 	void FixedUpdate () {
-		float t = (Mathf.Sin(Time.time)+1.0f)/2.0f;
+		float t = (Mathf.Sin(Time.time)+1.0f)/movementTime;
 		rigidbody2D.MovePosition( sourcePos + Vector3.Lerp(Vector3.zero, offsetPos, t));
 	}
 }

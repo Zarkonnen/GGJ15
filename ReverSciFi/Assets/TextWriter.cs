cd148477c3060090b9409d8c500566b16c1018d6
Reto Spoerri <spoerri@ludic.ch>
2015-01-25 13:50:42 +0100

    intro update, text code fixes

--- ReverSciFi/Assets/TextWriter.cs
+++ ReverSciFi/Assets/TextWriter.cs
@@ -38,12 +38,16 @@ public class TextWriter : MonoBehaviour {
 	void Update () {
 		/*Vector3 charScreenPos = mainCamera.WorldToScreenPoint(character.transform.position);
 		Vector2 textScreenPos = GUIUtility.ScreenToGUIPoint(new Vector2(charScreenPos.x, charScreenPos.y));
-		transform.position = textScreenPos;
+		transform.position = textScreenPos;*/
+		//transform.parent.GetComponent<Canvas>().pixelRect = new Rect(transform.position.x, transform.position.y, 200, 200);
+		if (transform.lossyScale.x < 0) {
+			transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+		}
 		timer -= Time.deltaTime;
 		if (timer < 0.0f) {
 			stringId += 1;
 			timer = timePerText;
 			GetComponent<Text>().text = texte[stringId];
-		}*/
+		}
 	}
 }
